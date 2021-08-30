34dc5c561ec597d996f4f09548d4356276ffa742
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-08-30 19:42:58 +0200

    PDE problems

---
+++ src/pnmol/pde_problems.py
@@ -0,0 +1,59 @@
+from collections import namedtuple
+
+import jax.numpy as jnp
+import scipy.stats
+
+from pnmol import differential_operator, discretize, kernels, mesh
+
+
+class DiscretizedPDE(
+    namedtuple(
+        "_DiscretizedPDE",
+        "f t0 tmax y0 df E",
+        defaults=(None, None),
+    )
+):
+    """Initial value problems."""
+
+    @property
+    def dimension(self):
+        if jnp.isscalar(self.y0):
+            return 1
+        return self.y0.shape[0]
+
+    @property
+    def t_span(self):
+        return self.t0, self.tmax
+
+
+def heat_1d(bbox=None, dx=0.02, stencil_size=3, t0=0.0, tmax=20.0, y0=None):
+    # Bounding box for spatial discretization grid
+    if bbox is None:
+        bbox = [0.0, 1.0]
+    bbox = jnp.asarray(bbox)
+    assert bbox.ndim == 1
+
+    # Create spatial discretization grid
+    grid = mesh.RectangularMesh.from_bounding_boxes_1d(bounding_boxes=bbox, step=dx)
+
+    # Spatial initial condition at t=0
+    if y0 is None:
+        y0 = jnp.array(
+            scipy.stats.norm(0.5 * (bbox[1] - bbox[0]), 0.05).pdf(
+                grid.points.reshape(-1)
+            )
+        )
+        y0 = y0 / y0.max()
+
+    # PNMOL discretization
+    lengthscale = dx * int(stencil_size / 2)
+    gauss_kernel = kernels.GaussianKernel(lengthscale)
+    laplace = differential_operator.laplace()
+    L, E = discretize.discretize(
+        diffop=laplace, mesh=grid, kernel=gauss_kernel, stencil_size=stencil_size
+    )
+
+    f = lambda t, x: L @ x
+    df = lambda t, x: L
+
+    return DiscretizedPDE(f=f, t0=t0, tmax=tmax, y0=y0, df=df, E=E)
