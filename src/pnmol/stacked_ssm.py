83506515237d710e38cf605ff742f13bd188959d
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-09-16 14:46:20 +0200

    First approach to stacked SSMs

--- src/pnmol/stacked_ssm.py
+++ src/pnmol/stacked_ssm.py
@@ -79,3 +79,66 @@ class BlockDiagonal:
                 raise ValueError("BlockDiagonal matmul is not supported for n_dim > 2.")
 
         return NotImplemented
+
+
+class StackedSSM:
+    def __init__(self, processes) -> None:
+        self.processes = tuple(processes)
+        self._dims = tuple((p.state_dimension for p in self.processes))
+
+    @property
+    def state_dimension(self):
+        return sum(self._dims)
+
+    @property
+    def preconditioned_discretize(self):
+        As, Qs = [], []
+        for p in self.processes:
+            A, SQ = p.preconditioned_discretize
+            As.append(A)
+            Qs.append(SQ @ SQ.T)
+
+        A = jax.scipy.linalg.block_diagonal(*As)
+        Q = jax.scipy.linalg.block_diagonal(*Qs)
+        return A, jnp.linalg.cholesky(Q)
+
+    def nordsieck_preconditioner(self, dt):
+        Ps, P_invs = [], []
+        for p in self.processes:
+            prec, prec_inv = p.nordsieck_preconditioner(dt)
+            Ps.append(prec)
+            P_invs.append(prec_inv)
+        P = jax.scipy.linalg.block_diagonal(*Ps)
+        P_inv = jax.scipy.linalg.block_diagonal(*P_invs)
+        return P, P_inv
+
+    def projection_matrix(
+        self, derivative_to_project_onto, process_to_project_onto=None
+    ):
+        if process_to_project_onto is None:
+            return jax.scipy.linalg.block_diagonal(
+                *[
+                    p.projection_matrix(derivative_to_project_onto)
+                    for p in self.processes
+                ]
+            )
+
+        assert isinstance(process_to_project_onto, int)
+        proj_to_proc = self.projection_to_process(process_to_project_onto)
+        proj_to_deriv = self.processes[process_to_project_onto].projection_matrix(
+            derivative_to_project_onto
+        )
+        return proj_to_deriv @ proj_to_proc
+
+    def projection_to_process(self, process_to_project_onto: int):
+        start = (
+            sum(self._dims[0:process_to_project_onto])
+            if process_to_project_onto > 0
+            else 0
+        )
+        stop = (
+            start + self._dims[process_to_project_onto]
+            if process_to_project_onto < len(self.processes)
+            else None
+        )
+        return jnp.eye(self.state_dimension)[start:stop, :]
