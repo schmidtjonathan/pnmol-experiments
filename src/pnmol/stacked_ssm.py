14838a91e91881808d7e3311d2048e6a95e0b28e
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-09-20 18:25:25 +0200

    Make BlockDiagonal a valid pytree object

--- src/pnmol/stacked_ssm.py
+++ src/pnmol/stacked_ssm.py
@@ -1,30 +1,25 @@
+from collections import namedtuple
+
 import jax.numpy as jnp
 import jax.scipy.linalg
 import tornadox
 
 
-class BlockDiagonal:
-    def __init__(self, array_list):
-        self._array_list = array_list
-
-    @property
-    def array_list(self):
-        return self._array_list
-
+class BlockDiagonal(namedtuple("_BlockDiagonal", ["array_list"])):
     @property
     def num_blocks(self):
-        return len(self._array_list)
+        return len(self.array_list)
 
     @property
     def shape(self):
         return (
-            sum((a.shape[0] for a in self._array_list)),
-            sum((a.shape[1] for a in self._array_list)),
+            sum((a.shape[0] for a in self.array_list)),
+            sum((a.shape[1] for a in self.array_list)),
         )
 
     @property
     def T(self):
-        transposed_array_list = [a.T for a in self._array_list]
+        transposed_array_list = [a.T for a in self.array_list]
         return BlockDiagonal(array_list=transposed_array_list)
 
     def todense(self):
@@ -33,19 +28,19 @@ class BlockDiagonal:
     def __matmul__(self, other):
         if isinstance(other, jnp.ndarray):
             split_indices_colwise = jnp.cumsum(
-                jnp.array([a.shape[1] for a in self._array_list])
+                jnp.array([a.shape[1] for a in self.array_list])
             )[:-1]
             if other.ndim == 1:
                 split_array = jnp.split(other, split_indices_colwise)
                 individual_products = [
-                    self._array_list[i] @ split_array[i] for i in range(self.num_blocks)
+                    self.array_list[i] @ split_array[i] for i in range(self.num_blocks)
                 ]
                 return jnp.concatenate(individual_products)
 
             elif other.ndim == 2:
                 in_block_rows = jnp.split(other, split_indices_colwise, axis=0)
                 out_block_rows = [
-                    self._array_list[block_diag_index] @ r
+                    self.array_list[block_diag_index] @ r
                     for block_diag_index, r in enumerate(in_block_rows)
                 ]
                 return jnp.concatenate(out_block_rows, axis=0)
@@ -58,19 +53,19 @@ class BlockDiagonal:
     def __rmatmul__(self, other):
         if isinstance(other, jnp.ndarray):
             split_indices_rowwise = jnp.cumsum(
-                jnp.array([a.shape[0] for a in self._array_list])
+                jnp.array([a.shape[0] for a in self.array_list])
             )[:-1]
             if other.ndim == 1:
                 split_array = jnp.split(other, split_indices_rowwise)
                 individual_products = [
-                    split_array[i] @ self._array_list[i] for i in range(self.num_blocks)
+                    split_array[i] @ self.array_list[i] for i in range(self.num_blocks)
                 ]
                 return jnp.concatenate(individual_products)
 
             elif other.ndim == 2:
                 in_block_cols = jnp.split(other, split_indices_rowwise, axis=1)
                 out_block_cols = [
-                    c @ self._array_list[block_diag_index]
+                    c @ self.array_list[block_diag_index]
                     for block_diag_index, c in enumerate(in_block_cols)
                 ]
                 return jnp.concatenate(out_block_cols, axis=1)
@@ -129,7 +124,7 @@ class StackedSSM:
         if process_to_project_onto is None:
             return [
                 p.projection_matrix(derivative_to_project_onto) for p in self.processes
-                ]
+            ]
 
         assert isinstance(process_to_project_onto, int)
         proj_to_proc = self.projection_to_process(process_to_project_onto)
