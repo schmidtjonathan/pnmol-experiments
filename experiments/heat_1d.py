9580277c32d23809a2631d5a24e68bd7003097c8
Nicholas Krämer <nicokraemer@gmx.net>
2021-09-24 10:00:07 +0200

    plotting

--- experiments/heat_1d.py
+++ experiments/heat_1d.py
@@ -9,6 +9,7 @@ from matplotlib import animation
 import pnmol
 
 ANIMATION = False
+SAVE = True
 
 # Create the PDE problem
 discretized_pde = pnmol.pde_problems.heat_1d(
@@ -28,10 +29,37 @@ cov = sol.cov_sqrtm @ jnp.transpose(sol.cov_sqrtm, axes=(0, 2, 1))
 stds = jnp.sqrt(jnp.diagonal(cov, axis1=1, axis2=2) @ E0.T)
 
 # Plot mean and std
-fig, (ax1, ax2) = plt.subplots(ncols=2, dpi=200)
-mean_map = ax1.imshow(means.T, cmap="cividis", vmin=0.0, vmax=1.0)
-std_map = ax2.imshow(jnp.log(stds.T), cmap="cividis")
-plt.tight_layout()
+
+xgrid = discretized_pde.spatial_grid.points.squeeze()
+tgrid = sol.t
+
+T, X = jnp.meshgrid(xgrid, tgrid)
+def plot_contour(ax, *args, **kwargs):
+    """Contour lines with fill color and sharp edges."""
+    ax.contour(*args, **kwargs)
+    ax.contourf(*args, **kwargs)
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, dpi=100, figsize=(8,3), sharex=True, sharey=True, constrained_layout=True)
+plot_contour(ax1, X, T, means, cmap="gist_rainbow", alpha=0.45, linewidths=0.8)
+plot_contour(ax2, X, T, stds, cmap="gist_rainbow", alpha=0.45, linewidths=0.8)
+
+
+ax1.set_title(r"$\bf a.$ " + "Means", loc="left")
+ax2.set_title(r"$\bf b.$ " + "Standard deviations", loc="left")
+
+for ax in [ax1, ax2]:
+    ax.set_xlabel("Time, $t$")
+    ax.set_xticks(tgrid)
+    ax.set_yticks(xgrid)
+    ax.set_xticklabels(())
+    ax.set_yticklabels(())
+ax1.set_ylabel("Space, $x$")
+
+ax1.grid(which="major", color="k", alpha=0.25, linestyle="dotted")
+ax2.grid(which="major", color="k", alpha=0.25, linestyle="dotted")
+
+if SAVE:
+    plt.savefig("means_and_stds.pdf")
 plt.show()
 
 
