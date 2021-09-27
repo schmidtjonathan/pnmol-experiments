7e4d0ffc525f7720faa0d0596eca17a795b49ea9
Nicholas Krämer <nicokraemer@gmx.net>
2021-09-27 14:26:26 +0200

    test for discretise

--- src/pnmol/discretize.py
+++ src/pnmol/discretize.py
@@ -39,10 +39,10 @@ def discretize(diffop, mesh, kernel, stencil_size, cov_damping=0.0):
 
     M = len(mesh)
 
-    L_k = diffop(kernel, argnums=0)  # derivative function of kernel
+    L_k = diffop(kernel._evaluate_scalar, argnums=0)  # derivative function of kernel
     LL_k = diffop(L_k, argnums=1)
-    L_kx = kernels.LambdaKernel(fun=L_k)
-    LL_kx = kernels.LambdaKernel(fun=LL_k)
+    L_kx = kernels.Lambda(fun=L_k)
+    LL_kx = kernels.Lambda(fun=LL_k)
 
     fd_coeff_fun = partial(
         fd_coeff, grid=mesh, stencil_size=stencil_size, k=kernel, L_k=L_kx, LL_k=LL_kx
@@ -77,10 +77,8 @@ def fd_coeff(x, grid, stencil_size, k, L_k, LL_k, cov_damping):
     neighbors, neighbor_indices = grid.neighbours(point=x, num=stencil_size)
 
     X = neighbors.points
-    gram_matrix = k(X, X) + cov_damping * jnp.eye(X.shape[0])
-    diffop_at_point = L_k(x, X).reshape((-1,))
-
+    gram_matrix = k(X, X.T) + cov_damping * jnp.eye(X.shape[0])
+    diffop_at_point = L_k(x[None, :], X.T).reshape((-1,))
     weights = jnp.linalg.solve(gram_matrix, diffop_at_point)
     uncertainty = LL_k(x, x).reshape(()) - weights @ diffop_at_point
-
     return weights, uncertainty, neighbor_indices
---
+++ tests/test_discretize.py
@@ -0,0 +1,64 @@
+"""Tests for discretisation."""
+
+import jax.numpy as jnp
+import pytest
+
+from pnmol import differential_operator, discretize, kernels, mesh
+
+
+@pytest.fixture
+def bbox():
+    return jnp.array([0.0, 1.0])
+
+
+@pytest.fixture
+def dx():
+    return 0.1
+
+
+@pytest.fixture
+def grid_1d(bbox, dx):
+    return mesh.RectangularMesh.from_bounding_boxes_1d(bbox, dx)
+
+
+@pytest.fixture
+def diffop():
+    return differential_operator.laplace()
+
+
+@pytest.fixture
+def k():
+    return kernels.Polynomial()
+
+
+@pytest.fixture
+def L_k(k, diffop):
+    return kernels.Lambda(diffop(k.pairwise, argnums=0))
+
+
+@pytest.fixture
+def LL_k(L_k, diffop):
+    return kernels.Lambda(diffop(L_k.pairwise, argnums=1))
+
+
+@pytest.fixture
+def x0(grid_1d):
+    return grid_1d[0]
+
+
+@pytest.fixture
+def fd_coeff(x0, grid_1d, k, L_k, LL_k, dx):
+    return discretize.fd_coeff(
+        x=x0, grid=grid_1d, stencil_size=3, k=k, L_k=L_k, LL_k=LL_k, cov_damping=0.0
+    )
+
+
+def test_polynomial_weights_1_2_1(fd_coeff, dx):
+    weights_normalized = fd_coeff[0] * dx ** 2
+    assert jnp.allclose(weights_normalized, jnp.array([1.0, -2.0, 1.0]))
+
+
+def test_polynomial_uncertainty_zero(fd_coeff):
+
+    uncertainty = fd_coeff[1]
+    assert jnp.allclose(uncertainty, jnp.array(0.0))
