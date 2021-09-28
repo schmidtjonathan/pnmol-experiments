df97cb7017c186c24cf410932339e0b96e26903a
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-09-28 18:48:08 +0200

    Linear latent force EK1

--- src/pnmol/lfsolver.py
+++ src/pnmol/lfsolver.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+import abc
 from functools import partial
 
 import jax
@@ -7,41 +7,61 @@ import jax.numpy as jnp
 from pnmol import iwp, odefilter, rv, sqrt, stacked_ssm
 
 
-class LatentForceEK1(odefilter.ODEFilter):
+@partial(jax.jit, static_argnums=1)
+def extract_blocks_from_block_diag(block_diag_mat, num_blocks):
+    """ATTENTION: ASSUMES EQUAL-SIZED SQUARE BLOCKS!"""
+    block_rows = jnp.split(block_diag_mat, num_blocks, axis=0)
+    return [
+        jnp.split(
+            block_rows[i],
+            num_blocks,
+            axis=1,
+        )[i]
+        for i in range(num_blocks)
+    ]
+
+
+class LatentForceEK1Base(odefilter.ODEFilter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.P0 = None
         self.E0 = None
         self.E1 = None
 
-    def initialize(self, ivp):
+    def initialize(self, discretized_pde):
+
+        X = discretized_pde.spatial_grid.points
+        diffusion_state_sqrtm = jnp.linalg.cholesky(self.spatial_kernel(X, X.T))
 
         self.state_iwp = iwp.IntegratedWienerTransition(
             num_derivatives=self.num_derivatives,
-            wiener_process_dimension=ivp.dimension,
-            wp_diffusion_sqrtm=ivp.Kxx_sqrtm,
+            wiener_process_dimension=discretized_pde.dimension,
+            wp_diffusion_sqrtm=discretized_pde.Kxx_sqrtm,
         )
         self.lf_iwp = iwp.IntegratedWienerTransition(
             num_derivatives=self.num_derivatives,
-            wiener_process_dimension=ivp.dimension,
-            wp_diffusion_sqrtm=ivp.E_sqrtm,
+            wiener_process_dimension=discretized_pde.dimension,
+            wp_diffusion_sqrtm=discretized_pde.E_sqrtm,
         )
         self.ssm = stacked_ssm.StackedSSM(processes=[self.state_iwp, self.lf_iwp])
 
-        self.P0 = self.E0 = self.ssm.projection_matrix(0)
-        self.E1 = self.ssm.projection_matrix(1)
+        self.P0 = self.E0 = self.state_iwp.projection_matrix(0)
+        self.E1 = self.state_iwp.projection_matrix(1)
 
         extended_dy0, cov_sqrtm_state = self.init(
-            f=ivp.f,
-            df=ivp.df,
-            y0=ivp.y0,
-            t0=ivp.t0,
+            f=discretized_pde.f,
+            df=discretized_pde.df,
+            y0=discretized_pde.y0,
+            t0=discretized_pde.t0,
             num_derivatives=self.state_iwp.num_derivatives,
+            wp_diffusion_sqrtm=diffusion_state_sqrtm,
         )
         mean = jnp.concatenate([extended_dy0, jnp.zeros_like(extended_dy0)], -1)
 
-        cov_sqrtm_state = jnp.kron(ivp.Kxx_sqrtm, cov_sqrtm_state)
-        cov_sqrtm_eps = jnp.kron(ivp.E_sqrtm, 1e-10 * jnp.eye(self.num_derivatives + 1))
+        cov_sqrtm_state = jnp.kron(discretized_pde.Kxx_sqrtm, cov_sqrtm_state)
+        cov_sqrtm_eps = jnp.kron(
+            discretized_pde.E_sqrtm, 1e-10 * jnp.eye(self.num_derivatives + 1)
+        )
 
         cov_sqrtm = jax.scipy.linalg.block_diag(
             cov_sqrtm_state,
@@ -51,7 +71,7 @@ class LatentForceEK1(odefilter.ODEFilter):
         y = rv.MultivariateNormal(mean=mean, cov_sqrtm=cov_sqrtm)
 
         return odefilter.ODEFilterState(
-            t=ivp.t0,
+            t=discretized_pde.t0,
             y=y,
             error_estimate=None,
             reference_state=None,
@@ -72,13 +92,11 @@ class LatentForceEK1(odefilter.ODEFilter):
 
         # Measure / calibrate
         z, H = self.evaluate_ode(
-            f=discretized_pde.f,
-            df=discretized_pde.df,
+            discretized_pde=discretized_pde,
             p0=self.E0,
             p1=self.E1,
             m_pred=mp,
             t=state.t + dt,
-            B=discretized_pde.spatial_grid.boundary_projection_matrix,
         )
 
         Cl = state.y.cov_sqrtm
@@ -110,36 +128,27 @@ class LatentForceEK1(odefilter.ODEFilter):
     def predict_mean(A, m):
         return A @ m
 
-    @staticmethod
-    @partial(jax.jit, static_argnums=1)
-    def extract_blocks_from_block_diag(block_diag_mat, num_blocks):
-        """ATTENTION: ASSUMES EQUAL-SIZED SQUARE BLOCKS!"""
-        block_rows = jnp.split(block_diag_mat, num_blocks, axis=0)
-        return [
-            jnp.split(
-                block_rows[i],
-                num_blocks,
-                axis=1,
-            )[i]
-            for i in range(num_blocks)
-        ]
+    @abc.abstractstaticmethod
+    def evaluate_ode(*args, **kwargs):
+        pass
 
-    @staticmethod
-    @partial(jax.jit, static_argnums=(0, 1))
-    def evaluate_ode(f, df, p0, p1, m_pred, t, B):
 
-        m_at = p0 @ m_pred  # Project to first derivatives
+class LinearLatentForceEK1(LatentForceEK1Base):
+    @staticmethod
+    @partial(jax.jit, static_argnums=(0,))
+    def evaluate_ode(discretized_pde, p0, p1, m_pred, t):
+        L = discretized_pde.L
+        B = discretized_pde.spatial_grid.boundary_projection_matrix
+
+        E0_state = E0_eps = p0
+        E1_state = p1
+        E0_stacked = jax.scipy.linalg.block_diag(E0_state, E0_eps)
+
+        m_at = E0_stacked @ m_pred  # Project to first derivatives
         state_at, eps_at = jnp.split(m_at, 2)  # Split up into ODE state and error
 
-        fx = f(t, state_at)  # Evaluate vector field
-        Jx = df(t, state_at)  # Evaluate Jacobian of the vector field
-
-        E0_state, E0_eps = LatentForceEK1.extract_blocks_from_block_diag(
-            p0, num_blocks=2
-        )
-        E1_state, E1_eps = LatentForceEK1.extract_blocks_from_block_diag(
-            p1, num_blocks=2
-        )
+        fx = L @ state_at  # Evaluate vector field
+        Jx = L  # Evaluate Jacobian of the vector field
 
         H_state = E1_state - Jx @ E0_state
         H_eps = -E0_eps
