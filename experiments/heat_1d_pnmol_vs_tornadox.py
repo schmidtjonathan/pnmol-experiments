b347a870d80e55caaa649fc38454cb3e3aad1c9a
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-09-28 17:24:21 +0200

    fig1 plot

--- experiments/heat_1d_pnmol_vs_tornadox.py
+++ experiments/heat_1d_pnmol_vs_tornadox.py
@@ -10,10 +10,14 @@ SAVE = True
 
 
 def solve_pde_pnmol(pde, dt, nu, progressbar, kernel):
+    print()
+    print("PNMOL")
     steprule = pnmol.step.ConstantSteps(dt)
 
     # Solve the discretised PDE
-    ek1 = pnmol.solver.MeasurementCovarianceEK1(num_derivatives=nu, steprule=steprule)
+    ek1 = pnmol.solver.MeasurementCovarianceEK1(
+        num_derivatives=nu, steprule=steprule, spatial_kernel=kernel
+    )
     sol = ek1.solve(pde, progressbar=progressbar)
 
     E0 = ek1.iwp.projection_matrix(0)
@@ -21,6 +25,8 @@ def solve_pde_pnmol(pde, dt, nu, progressbar, kernel):
 
 
 def solve_pde_tornadox(pde, dt, nu, progressbar):
+    print()
+    print("TORNADOX")
     steprule = tornadox.step.ConstantSteps(dt)
     ivp = pde.to_tornadox_ivp_1d()
     ek1 = tornadox.ek1.ReferenceEK1(
@@ -35,6 +41,26 @@ def solve_pde_tornadox(pde, dt, nu, progressbar):
     return (means, stds), sol.t
 
 
+def solve_pde_reference(pde, dt, nu, progressbar):
+    print()
+    print("REFERENCE")
+    steprule = tornadox.step.ConstantSteps(dt)
+    ivp = pde.to_tornadox_ivp_1d()
+    ek1 = tornadox.ek1.ReferenceEK1(
+        num_derivatives=nu, steprule=steprule, initialization=tornadox.init.RungeKutta()
+    )
+    sol = ek1.solve(ivp, progressbar=progressbar)
+    E0 = ek1.iwp.projection_matrix(0)
+    means, stds = read_mean_and_std(sol, E0)
+
+    means = jnp.pad(means, pad_width=1, mode="constant", constant_values=0.0)[1:-1, ...]
+    stds = jnp.pad(stds, pad_width=1, mode="constant", constant_values=0.0)[1:-1, ...]
+    return (
+        means[::high_res_factor, ::high_res_factor],
+        stds[::high_res_factor, ::high_res_factor],
+    ), sol.t[::high_res_factor]
+
+
 def read_mean_and_std(sol, E0):
     means = sol.mean[:, 0]
     cov = sol.cov_sqrtm @ jnp.transpose(sol.cov_sqrtm, axes=(0, 2, 1))
@@ -42,22 +68,50 @@ def read_mean_and_std(sol, E0):
     return means, stds
 
 
-kernel = pnmol.kernels.Polynomial()
-discretized_pde = pnmol.pde_problems.heat_1d(
+dt = 0.05
+dx = 0.2
+high_res_factor = 16
+kernel = pnmol.kernels.SquareExponential()
+discretized_pde_pnmol = pnmol.pde_problems.heat_1d(
     tmax=5.0,
-    dx=0.1,
+    dx=dx,
     stencil_size=3,
     diffusion_rate=0.05,
-    kernel=kernel,
+    kernel=pnmol.kernels.SquareExponential(),
+    cov_damping_fd=0.0,
+    cov_damping_diffusion=1.0,
+)
+discretized_pde_tornadox = pnmol.pde_problems.heat_1d(
+    tmax=5.0,
+    dx=dx,
+    stencil_size=3,
+    diffusion_rate=0.05,
+    kernel=pnmol.kernels.Polynomial(),
+    cov_damping_fd=0.0,
+    cov_damping_diffusion=1.0,
+)
+discretized_pde_high_res = pnmol.pde_problems.heat_1d(
+    tmax=5.0,
+    dx=dx / high_res_factor,
+    stencil_size=3,
+    diffusion_rate=0.05,
+    kernel=pnmol.kernels.Polynomial(),
     cov_damping_fd=0.0,
     cov_damping_diffusion=1.0,
 )
 
-dt = 0.1
 nu = 2
-res_pnmol = solve_pde_pnmol(discretized_pde, dt, nu, True, kernel)
-res_tornadox = solve_pde_tornadox(discretized_pde, dt, nu, True)
-
+res_pnmol = solve_pde_pnmol(
+    discretized_pde_pnmol,
+    dt,
+    nu,
+    True,
+    pnmol.kernels.Matern52() + pnmol.kernels.WhiteNoise(1e-4),
+)
+res_tornadox = solve_pde_tornadox(discretized_pde_tornadox, dt, nu, True)
+res_reference = solve_pde_reference(
+    discretized_pde_high_res, dt / high_res_factor, nu, True
+)
 
 # Plot mean and std
 
@@ -74,13 +128,14 @@ def infer_vrange_std(res):
 
 
 # Need this later
-xgrid = discretized_pde.spatial_grid.points.squeeze()
+xgrid = discretized_pde_pnmol.spatial_grid.points.squeeze()
+xgrid2 = discretized_pde_high_res.spatial_grid.points.squeeze()
 
 # Create 2x2 grid
 fig, axes = plt.subplots(
-    ncols=2,
+    ncols=3,
     nrows=2,
-    dpi=100,
+    dpi=400,
     figsize=(8, 4),
     sharex=True,
     sharey=True,
@@ -92,8 +147,12 @@ fig, axes = plt.subplots(
 # Reversed so the "last" colormap is the first row (PNMOL),
 # which is set to be the figures colorbar below.
 vmin_std, vmax_std = infer_vrange_std(res_pnmol)
-for row_axes, res in zip(reversed(axes), reversed([res_pnmol, res_tornadox])):
-    ax1, ax2 = row_axes
+
+(means_ref, _), _ = res_reference
+for i, (row_axes, res) in enumerate(
+    zip(reversed(axes), reversed([res_pnmol, res_tornadox]))
+):
+    ax1, ax2, ax3 = row_axes
     (means, stds), tgrid = res
     T, X = jnp.meshgrid(xgrid, tgrid)
     plot_contour(
@@ -102,7 +161,6 @@ for row_axes, res in zip(reversed(axes), reversed([res_pnmol, res_tornadox])):
         T,
         means,
         cmap="Greys",
-        levels=10,
         alpha=0.45,
         linewidths=0.8,
         vmin=0,
@@ -113,8 +171,19 @@ for row_axes, res in zip(reversed(axes), reversed([res_pnmol, res_tornadox])):
         X,
         T,
         stds,
-        cmap="gist_rainbow",
-        levels=10,
+        cmap="seismic",
+        alpha=0.45,
+        linewidths=0.8,
+        vmin=vmin_std,
+        vmax=vmax_std,
+    )
+    n = jnp.minimum(len(means), len(means_ref))
+    plot_contour(
+        ax3,
+        X[:n],
+        T[:n],
+        jnp.abs(means[:n] - means_ref[:n]),
+        cmap="seismic",
         alpha=0.45,
         linewidths=0.8,
         vmin=vmin_std,
@@ -124,15 +193,16 @@ for row_axes, res in zip(reversed(axes), reversed([res_pnmol, res_tornadox])):
 # Create a colorbar based on the PNMOL output (which is why we reversed the loop above)
 fig.colorbar(
     colors,
-    ax=axes[:, 1].ravel().tolist(),
+    ax=axes[:, -1].ravel().tolist(),
     ticks=(vmin_std, 0.5 * (vmin_std + vmax_std), vmax_std),
 )
 
 # Column titles
 top_row_axis = axes[0]
-ax1, ax2 = top_row_axis
+ax1, ax2, ax3 = top_row_axis
 ax1.set_title(r"$\bf a.$ " + "Means", loc="left")
 ax2.set_title(r"$\bf b.$ " + "Standard deviations", loc="left")
+ax3.set_title(r"$\bf c.$ " + "Error", loc="left")
 
 # x-labels
 bottom_row_axis = axes[1]
