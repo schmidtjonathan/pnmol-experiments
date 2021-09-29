bdf0f674107bfa26bbf9587d7d7a223a231d64ba
Nicholas Krämer <nicokraemer@gmx.net>
2021-09-29 16:23:55 +0200

    moved to decorator

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -3,20 +3,19 @@ import pytest
 
 import pnmol
 
-
-@pytest.fixture
-def prob1d():
-    return pnmol.problems.heat_1d()
-
-
-@pytest.fixture
-def mesh_spatial(prob1d):
-    return pnmol.mesh.RectangularMesh.from_bounding_boxes_1d(prob1d.bbox, step=0.1)
+problems_1d_all = pytest.mark.parametrize(
+    "prob1d", [pnmol.problems.heat_1d(bcond="dirichlet")]
+)
 
 
 @pytest.fixture
 def kernel():
     return pnmol.kernels.SquareExponential()
+
+
+@pytest.fixture
+def mesh_spatial(prob1d):
+    return pnmol.mesh.RectangularMesh.from_bounding_boxes_1d(prob1d.bbox, step=0.1)
 
 
 class TestProb1dDiscretized:
@@ -33,18 +32,22 @@ class TestProb1dDiscretized:
         return prob1d
 
     @staticmethod
+    @problems_1d_all
     def test_type(prob1d_discretized):
         assert isinstance(prob1d_discretized, pnmol.problems.LinearPDE)
 
     @staticmethod
+    @problems_1d_all
     def test_t0(prob1d_discretized):
         assert jnp.isscalar(prob1d_discretized.t0)
 
     @staticmethod
+    @problems_1d_all
     def test_tmax(prob1d_discretized):
         assert jnp.isscalar(prob1d_discretized.t0)
 
     @staticmethod
+    @problems_1d_all
     def test_y0(prob1d_discretized):
         N = prob1d_discretized.mesh_spatial.shape[0]
         assert prob1d_discretized.y0_array.shape == (N,)
