f7b37c45c3972da09526379594f373eedeff4121
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-10-07 12:14:16 +0200

    Add reference, fix saving results

--- experiments/figure_main.py
+++ experiments/figure_main.py
@@ -12,6 +12,25 @@ import tqdm
 import pnmol
 
 
+def solve_pde_reference(pde, *, dt, high_res_factor_dx, high_res_factor_dt):
+    t_eval = jnp.array([pde.tmax])
+    ivp = pde.to_tornadox_ivp()
+    sol = scipy.integrate.solve_ivp(ivp.f, ivp.t_span, ivp.y0, t_eval=t_eval)
+
+    means = sol.y.T
+    assert means.shape == (1, ivp.y0.size)
+    stds = 0.0 * sol.y.T
+
+    means = jnp.pad(means, pad_width=1, mode="constant", constant_values=0.0)[
+        1:-1, ...
+    ][::high_res_factor_dt, ::high_res_factor_dx]
+    stds = jnp.pad(stds, pad_width=1, mode="constant", constant_values=0.0)[1:-1, ...][
+        ::high_res_factor_dt, ::high_res_factor_dx
+    ]
+
+    return means, stds, t_eval[0], pde.mesh_spatial.points[::high_res_factor_dx], -1
+
+
 def solve_pde_pnmol_white(pde, *, dt, nu, progressbar, kernel):
     steprule = pnmol.odetools.step.Constant(dt)
     ek1 = pnmol.white.LinearWhiteNoiseEK1(
@@ -21,19 +40,19 @@ def solve_pde_pnmol_white(pde, *, dt, nu, progressbar, kernel):
     E0 = ek1.iwp.projection_matrix(0)
     mean, std = read_mean_and_std(final_state, E0)
 
-    return mean, std, final_state.t, pde.mesh_spatial.points, info
+    return mean, std, final_state.t, pde.mesh_spatial.points, info["num_steps"]
 
 
-def solve_pde_pnmol_latent(pde, *, dt, nu, progressbar, kernel):
-    steprule = pnmol.odetools.step.Constant(dt)
-    ek1 = pnmol.latent.LinearLatentForceEK1(
-        num_derivatives=nu, steprule=steprule, spatial_kernel=kernel
-    )
-    final_state, info = ek1.simulate_final_state(pde, progressbar=progressbar)
-    E0 = ek1.state_iwp.projection_matrix(0)
-    mean, std = read_mean_and_std_latent(final_state, E0)
+# def solve_pde_pnmol_latent(pde, *, dt, nu, progressbar, kernel):
+#     steprule = pnmol.odetools.step.Constant(dt)
+#     ek1 = pnmol.latent.LinearLatentForceEK1(
+#         num_derivatives=nu, steprule=steprule, spatial_kernel=kernel
+#     )
+#     final_state, info = ek1.simulate_final_state(pde, progressbar=progressbar)
+#     E0 = ek1.state_iwp.projection_matrix(0)
+#     mean, std = read_mean_and_std_latent(final_state, E0)
 
-    return mean, std, final_state.t, pde.mesh_spatial.points, info
+#     return mean, std, final_state.t, pde.mesh_spatial.points, info["num_steps"]
 
 
 def solve_pde_tornadox(pde, *, dt, nu, progressbar):
@@ -48,7 +67,7 @@ def solve_pde_tornadox(pde, *, dt, nu, progressbar):
 
     mean = jnp.pad(mean, pad_width=1, mode="constant", constant_values=0.0)[1:-1, ...]
     std = jnp.pad(std, pad_width=1, mode="constant", constant_values=0.0)[1:-1, ...]
-    return mean, std, final_state.t, pde.mesh_spatial.points, info
+    return mean, std, final_state.t, pde.mesh_spatial.points, info["num_steps"]
 
 
 def read_mean_and_std(final_state, E0):
@@ -75,8 +94,12 @@ def save_result(result, /, *, prefix, path="experiments/results"):
     if not path.is_dir():
         path.mkdir(parents=True)
 
-    means, stds, ts, xs, info = result
-    n_steps = info["num_steps"]
+    means = {k: v[0] for (k, v) in result.items()}
+    stds = {k: v[1] for (k, v) in result.items()}
+    ts = {k: v[2] for (k, v) in result.items()}
+    xs = {k: v[3] for (k, v) in result.items()}
+    n_steps = {k: v[4] for (k, v) in result.items()}
+
     path_means = path / (prefix + "_means.npy")
     path_stds = path / (prefix + "_stds.npy")
     path_ts = path / (prefix + "_ts.npy")
@@ -107,6 +130,9 @@ T0, TMAX = 0.0, 4.0
 DIFFUSION_RATE = 0.035
 
 
+RESULT_PNMOL_WHITE, RESULT_TORNADOX, RESULT_REFERENCE = {}, {}, {}
+
+
 for exp_i, (dt, dx) in enumerate(itertools.product(DTs, DXs), start=1):
 
     print(f"\n======| Experiment {exp_i}: dt={dt}, dx={dx} \n")
@@ -132,28 +158,45 @@ for exp_i, (dt, dx) in enumerate(itertools.product(DTs, DXs), start=1):
         nugget_gram_matrix_fd=NUGGET_COV_FD,
         bcond="dirichlet",
     )
+    PDE_REFERENCE = pnmol.pde.examples.heat_1d_discretized(
+        t0=T0,
+        tmax=TMAX,
+        dx=dx / HIGH_RES_FACTOR_DX,
+        stencil_size=STENCIL_SIZE,
+        diffusion_rate=DIFFUSION_RATE,
+        kernel=pnmol.kernels.SquareExponential(),
+        nugget_gram_matrix_fd=NUGGET_COV_FD,
+        bcond="dirichlet",
+    )
 
     # Solve the PDE with the different methods
     KERNEL_NUGGET = pnmol.kernels.WhiteNoise(output_scale=1e-3)
     KERNEL_DIFFUSION_PNMOL = pnmol.kernels.Matern52() + KERNEL_NUGGET
-    RESULT_PNMOL_WHITE = solve_pde_pnmol_white(
+    res_white = solve_pde_pnmol_white(
         PDE_PNMOL,
         dt=dt,
         nu=NUM_DERIVATIVES,
         progressbar=PROGRESSBAR,
         kernel=KERNEL_DIFFUSION_PNMOL,
     )
-    # RESULT_PNMOL_LATENT = solve_pde_pnmol_latent(
-    #     PDE_PNMOL,
-    #     dt=dt,
-    #     nu=NUM_DERIVATIVES,
-    #     progressbar=PROGRESSBAR,
-    #     kernel=KERNEL_DIFFUSION_PNMOL,
-    # )
-    RESULT_TORNADOX = solve_pde_tornadox(
+
+    res_tornadox = solve_pde_tornadox(
         PDE_TORNADOX, dt=dt, nu=NUM_DERIVATIVES, progressbar=PROGRESSBAR
     )
+    res_reference = solve_pde_reference(
+        PDE_REFERENCE,
+        dt=dt / HIGH_RES_FACTOR_DT,
+        high_res_factor_dt=HIGH_RES_FACTOR_DT,
+        high_res_factor_dx=HIGH_RES_FACTOR_DX,
+    )
 
-    save_result(RESULT_PNMOL_WHITE, prefix="pnmol_white")
-    # save_result(RESULT_PNMOL_LATENT, prefix="pnmol_latent")
-    save_result(RESULT_TORNADOX, prefix="tornadox")
+    print(res_white[2], res_tornadox[2], res_reference[2])
+
+    RESULT_PNMOL_WHITE[(dt, dx)] = res_white
+    RESULT_TORNADOX[(dt, dx)] = res_tornadox
+    RESULT_REFERENCE[(dt, dx)] = res_reference
+
+
+save_result(RESULT_PNMOL_WHITE, prefix="pnmol_white")
+save_result(RESULT_TORNADOX, prefix="tornadox")
+save_result(RESULT_REFERENCE, prefix="reference")
