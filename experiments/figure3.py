cf64213ffe2916b7ea95a3225a900ff7f1dc5ae1
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-10-10 14:14:45 +0200

    Correct slicing and RMSE

--- experiments/figure3.py
+++ experiments/figure3.py
@@ -18,14 +18,21 @@ import pnmol
 def solve_pde_reference(pde, *, high_res_factor_dx):
     t_eval = jnp.array([pde.tmax])
     ivp = pde.to_tornadox_ivp()
-    sol = scipy.integrate.solve_ivp(ivp.f, ivp.t_span, ivp.y0, t_eval=t_eval)
+    sol = scipy.integrate.solve_ivp(
+        ivp.f, ivp.t_span, ivp.y0, t_eval=t_eval, atol=1e-10, rtol=1e-10
+    )
 
     mean = sol.y.T
     std = 0.0 * sol.y.T
     assert mean.shape == (1, ivp.y0.size) == std.shape
     mean, std = mean.squeeze(), std.squeeze()  # (highres * dx,)
 
-    mean, std = mean[::high_res_factor_dx], std[::high_res_factor_dx]
+    means = [
+        m[high_res_factor_dx - 1 :: high_res_factor_dx] for m in jnp.split(mean, 3)
+    ]  # (dx, )
+    stds = [s[high_res_factor_dx - 1 :: high_res_factor_dx] for s in jnp.split(std, 3)]
+    mean = jnp.concatenate(means)
+    std = jnp.concatenate(stds)
 
     return mean, std, -1
 
@@ -95,10 +102,14 @@ def save_result(result, /, *, prefix, path="experiments/results"):
     path_error = path / (prefix + "_error.npy")
     path_std = path / (prefix + "_std.npy")
     path_runtime = path / (prefix + "_runtime.npy")
+    path_dt = path / (prefix + "_dt.npy")
+    path_dx = path / (prefix + "_dx.npy")
 
     jnp.save(path_error, result["error"])
     jnp.save(path_std, result["std"])
     jnp.save(path_runtime, result["runtime"])
+    jnp.save(path_dt, result["dt"])
+    jnp.save(path_dx, result["dx"])
 
 
 def save_dtdx(path="experiments/results"):
@@ -133,7 +144,7 @@ STENCIL_SIZE = 3
 PROGRESSBAR = True
 
 # Hyperparameters (problem)
-T0, TMAX = 0.0, 40.0
+T0, TMAX = 0.0, 5.0
 DIFFUSION_RATE = 0.035
 
 
@@ -142,6 +153,8 @@ RESULT_WHITE, RESULT_TORNADOX = [
         "error": numpy.zeros((len(DXs), len(DTs))),
         "std": numpy.zeros((len(DXs), len(DTs))),
         "runtime": numpy.zeros((len(DXs), len(DTs))),
+        "dx": numpy.zeros((len(DXs), len(DTs))),
+        "dt": numpy.zeros((len(DXs), len(DTs))),
     }
     for _ in range(2)
 ]
@@ -152,52 +165,41 @@ num_exp_total = len(DXs) * len(DTs)
 
 save_dtdx()
 
+# Solve the PDE with the different methods
+KERNEL_DIFFUSION_PNMOL = pnmol.kernels.duplicate(
+    pnmol.kernels.Matern52() + pnmol.kernels.WhiteNoise(), num=3
+)
+
 for i_dx, dx in enumerate(DXs):
+    # PDE problems
+    PDE_PNMOL = pnmol.pde.examples.sir_1d_discretized(
+        t0=T0,
+        tmax=TMAX,
+        dx=dx,
+        stencil_size_interior=STENCIL_SIZE,
+        stencil_size_boundary=STENCIL_SIZE,
+        diffusion_rate_S=DIFFUSION_RATE,
+        diffusion_rate_I=DIFFUSION_RATE,
+        diffusion_rate_R=DIFFUSION_RATE,
+        nugget_gram_matrix_fd=NUGGET_COV_FD,
+    )
+    PDE_REFERENCE = pnmol.pde.examples.sir_1d_discretized(
+        t0=T0,
+        tmax=TMAX,
+        dx=dx / HIGH_RES_FACTOR_DX,
+        stencil_size_interior=STENCIL_SIZE,
+        stencil_size_boundary=STENCIL_SIZE,
+        diffusion_rate_S=DIFFUSION_RATE,
+        diffusion_rate_I=DIFFUSION_RATE,
+        diffusion_rate_R=DIFFUSION_RATE,
+        nugget_gram_matrix_fd=NUGGET_COV_FD,
+    )
     for i_dt, dt in enumerate(DTs):
         i_exp = i_exp + 1
 
-        # PDE problems
-        PDE_PNMOL = pnmol.pde.examples.sir_1d_discretized(
-            t0=T0,
-            tmax=TMAX,
-            dx=dx,
-            stencil_size_interior=STENCIL_SIZE,
-            stencil_size_boundary=STENCIL_SIZE,
-            diffusion_rate_S=DIFFUSION_RATE,
-            diffusion_rate_I=DIFFUSION_RATE,
-            diffusion_rate_R=DIFFUSION_RATE,
-            nugget_gram_matrix_fd=NUGGET_COV_FD,
-        )
-        PDE_TORNADOX = pnmol.pde.examples.sir_1d_discretized(
-            t0=T0,
-            tmax=TMAX,
-            dx=dx,
-            stencil_size_interior=STENCIL_SIZE,
-            stencil_size_boundary=STENCIL_SIZE,
-            diffusion_rate_S=DIFFUSION_RATE,
-            diffusion_rate_I=DIFFUSION_RATE,
-            diffusion_rate_R=DIFFUSION_RATE,
-            nugget_gram_matrix_fd=NUGGET_COV_FD,
-        )
-        PDE_REFERENCE = pnmol.pde.examples.sir_1d_discretized(
-            t0=T0,
-            tmax=TMAX,
-            dx=dx / HIGH_RES_FACTOR_DX,
-            stencil_size_interior=STENCIL_SIZE,
-            stencil_size_boundary=STENCIL_SIZE,
-            diffusion_rate_S=DIFFUSION_RATE,
-            diffusion_rate_I=DIFFUSION_RATE,
-            diffusion_rate_R=DIFFUSION_RATE,
-            nugget_gram_matrix_fd=NUGGET_COV_FD,
-        )
         dim = PDE_PNMOL.y0.size
         print(
             f"\n======| Experiment {i_exp} of {num_exp_total} +++ dt={dt}, dx={dx} (state dimension: {dim} = 3 * {dim//3}) \n"
-        )
-
-        # Solve the PDE with the different methods
-        KERNEL_DIFFUSION_PNMOL = pnmol.kernels.duplicate(
-            pnmol.kernels.Matern52() + pnmol.kernels.WhiteNoise(), num=3
         )
 
         mean_white, std_white, elapsed_time_white = solve_pde_pnmol_white(
@@ -209,15 +211,19 @@ for i_dx, dx in enumerate(DXs):
         )
 
         mean_tornadox, std_tornadox, elapsed_time_tornadox = solve_pde_tornadox(
-            PDE_TORNADOX, dt=dt, nu=NUM_DERIVATIVES, progressbar=PROGRESSBAR
+            PDE_PNMOL, dt=dt, nu=NUM_DERIVATIVES, progressbar=PROGRESSBAR
         )
         mean_reference, std_reference, elapsed_time_reference = solve_pde_reference(
             PDE_REFERENCE,
             high_res_factor_dx=HIGH_RES_FACTOR_DX,
         )
 
-        error_white = jnp.mean(jnp.abs(mean_white - mean_reference))
-        error_tornadox = jnp.mean(jnp.abs(mean_tornadox - mean_reference))
+        error_white = jnp.linalg.norm(
+            (mean_white - mean_reference) / mean_reference
+        ) / jnp.sqrt(mean_reference.size)
+        error_tornadox = jnp.linalg.norm(
+            (mean_tornadox - mean_reference) / mean_reference
+        ) / jnp.sqrt(mean_reference.size)
 
         mean_std_white = jnp.mean(std_white)
         mean_std_tornadox = jnp.mean(std_tornadox)
@@ -225,10 +231,14 @@ for i_dx, dx in enumerate(DXs):
         RESULT_WHITE["error"][i_dx, i_dt] = error_white
         RESULT_WHITE["std"][i_dx, i_dt] = mean_std_white
         RESULT_WHITE["runtime"][i_dx, i_dt] = elapsed_time_white
+        RESULT_WHITE["dt"][i_dx, i_dt] = dt
+        RESULT_WHITE["dx"][i_dx, i_dt] = dx
 
         RESULT_TORNADOX["error"][i_dx, i_dt] = error_tornadox
         RESULT_TORNADOX["std"][i_dx, i_dt] = mean_std_tornadox
         RESULT_TORNADOX["runtime"][i_dx, i_dt] = elapsed_time_tornadox
+        RESULT_TORNADOX["dt"][i_dx, i_dt] = dt
+        RESULT_TORNADOX["dx"][i_dx, i_dt] = dx
 
 
 save_result(RESULT_WHITE, prefix="pnmol_white")
