c899ba7a1a702e76e83399194a3bc181c68f1c29
Jonathan Schmidt <jonathan.schmidt@student.uni-tuebingen.de>
2021-10-10 15:36:46 +0200

    Chi2 and some corrections

--- experiments/figure3.py
+++ experiments/figure3.py
@@ -34,7 +34,12 @@ def solve_pde_reference(pde, *, high_res_factor_dx):
     mean = jnp.concatenate(means)
     std = jnp.concatenate(stds)
 
-    return mean, std, -1
+    return (
+        mean,
+        std,
+        None,
+        None,
+    )
 
 
 def solve_pde_pnmol_white(pde, *, dt, nu, progressbar, kernel):
@@ -48,21 +53,31 @@ def solve_pde_pnmol_white(pde, *, dt, nu, progressbar, kernel):
     elapsed_time = time.time() - start
 
     E0 = ek1.iwp.projection_matrix(0)
-    mean, std = read_mean_and_std(final_state, E0)
+    mean, std, cov = read_mean_and_std_and_cov(final_state, E0)
 
     means = [m[1:-1] for m in jnp.split(mean, 3)]  # (dx, )
     stds = [s[1:-1] for s in jnp.split(std, 3)]
+    blocks = [
+        [block[1:-1, 1:-1] for block in jnp.split(c_row, 3, axis=1)]
+        for c_row in jnp.split(cov, 3, axis=0)
+    ]
+
+    sliced_cov_rows = [jnp.concatenate(b, axis=1) for b in blocks]
+
+    sliced_cov = jnp.concatenate(sliced_cov_rows, axis=0)
     mean = jnp.concatenate(means)
     std = jnp.concatenate(stds)
 
-    return mean, std, elapsed_time
+    return mean, std, sliced_cov, elapsed_time
 
 
 def solve_pde_tornadox(pde, *, dt, nu, progressbar):
     steprule = tornadox.step.ConstantSteps(dt)
     ivp = pde.to_tornadox_ivp()
     ek1 = tornadox.ek1.ReferenceEK1(
-        num_derivatives=nu, steprule=steprule, initialization=tornadox.init.RungeKutta()
+        num_derivatives=nu,
+        steprule=steprule,
+        initialization=tornadox.init.Stack(use_df=False),
     )
 
     start = time.time()
@@ -70,28 +85,18 @@ def solve_pde_tornadox(pde, *, dt, nu, progressbar):
     elapsed_time = time.time() - start
 
     E0 = ek1.iwp.projection_matrix(0)
-    mean, std = read_mean_and_std(final_state, E0)
+    mean, std, cov = read_mean_and_std_and_cov(final_state, E0)
 
-    return mean, std, elapsed_time
+    return mean, std, cov, elapsed_time
 
 
-def read_mean_and_std(final_state, E0):
+def read_mean_and_std_and_cov(final_state, E0):
     # print("White")
     # print(final_state.y.mean.shape, final_state.y.cov_sqrtm.shape)
     mean = final_state.y.mean[0, :]
-    cov = final_state.y.cov_sqrtm @ final_state.y.cov_sqrtm.T
-    std = E0 @ jnp.sqrt(jnp.diagonal(cov))
-    return mean, std
-
-
-def read_mean_and_std_latent(final_state, E0):
-    # print("Latent")
-    # print(final_state.y.mean.shape, final_state.y.cov_sqrtm.shape)
-    mean, _ = jnp.split(final_state.y.mean[0, :], 2)
-    cov = final_state.y.cov_sqrtm @ final_state.y.cov_sqrtm.T
-    varis = jnp.diagonal(cov)
-    std = E0 @ jnp.sqrt(jnp.split(varis, 2)[0])
-    return mean, std
+    cov = E0 @ (final_state.y.cov_sqrtm @ final_state.y.cov_sqrtm.T) @ E0.T
+    std = jnp.sqrt(jnp.diagonal(cov))
+    return mean, std, cov
 
 
 def save_result(result, /, *, prefix, path="experiments/results"):
@@ -102,49 +107,41 @@ def save_result(result, /, *, prefix, path="experiments/results"):
     path_error = path / (prefix + "_error.npy")
     path_std = path / (prefix + "_std.npy")
     path_runtime = path / (prefix + "_runtime.npy")
+    path_chi2 = path / (prefix + "_chi2.npy")
     path_dt = path / (prefix + "_dt.npy")
     path_dx = path / (prefix + "_dx.npy")
 
     jnp.save(path_error, result["error"])
     jnp.save(path_std, result["std"])
     jnp.save(path_runtime, result["runtime"])
+    jnp.save(path_chi2, result["chi2"])
     jnp.save(path_dt, result["dt"])
     jnp.save(path_dx, result["dx"])
 
 
-def save_dtdx(path="experiments/results"):
-    path = pathlib.Path(path) / "figure3"
-    if not path.is_dir():
-        path.mkdir(parents=True)
-
-    dtdxsavepath = path / "dtdx.npy"
-    dtdx_array = jnp.stack((DTs, DXs))
-    jnp.save(dtdxsavepath, dtdx_array)
-    print(f"Saved dtdx of shape {dtdx_array.shape} to {dtdxsavepath}")
-
-
 # Ranges
 DTs = jnp.logspace(
-    # numpy.log10(0.001), numpy.log10(0.01), num=10, endpoint=True, base=10
+    # numpy.log10(0.001), numpy.log10(0.5), num=10, endpoint=True, base=10
     numpy.log10(0.05),
     numpy.log10(0.5),
     num=10,
     endpoint=True,
     base=10,
 )
-DXs = jnp.logspace(numpy.log10(0.1), numpy.log10(0.25), num=10, endpoint=True, base=10)
-
+DXs = jnp.array(
+    [0.01, 0.0125, 0.025, 0.05, 0.1, 0.2]
+)  # 1.0 / (2.0 ** jnp.arange(2, 5))
 
 # Hyperparameters (method)
 
-HIGH_RES_FACTOR_DX = 1
+HIGH_RES_FACTOR_DX = 4
 NUM_DERIVATIVES = 2
 NUGGET_COV_FD = 0.0
 STENCIL_SIZE = 3
 PROGRESSBAR = True
 
 # Hyperparameters (problem)
-T0, TMAX = 0.0, 5.0
+T0, TMAX = 0.0, 10.0
 DIFFUSION_RATE = 0.035
 
 
@@ -153,6 +150,7 @@ RESULT_WHITE, RESULT_TORNADOX = [
         "error": numpy.zeros((len(DXs), len(DTs))),
         "std": numpy.zeros((len(DXs), len(DTs))),
         "runtime": numpy.zeros((len(DXs), len(DTs))),
+        "chi2": numpy.zeros((len(DXs), len(DTs))),
         "dx": numpy.zeros((len(DXs), len(DTs))),
         "dt": numpy.zeros((len(DXs), len(DTs))),
     }
@@ -163,14 +161,12 @@ i_exp = 0
 num_exp_total = len(DXs) * len(DTs)
 
 
-save_dtdx()
-
 # Solve the PDE with the different methods
 KERNEL_DIFFUSION_PNMOL = pnmol.kernels.duplicate(
     pnmol.kernels.Matern52() + pnmol.kernels.WhiteNoise(), num=3
 )
 
-for i_dx, dx in enumerate(DXs):
+for i_dx, dx in enumerate(sorted(DXs)):
     # PDE problems
     PDE_PNMOL = pnmol.pde.examples.sir_1d_discretized(
         t0=T0,
@@ -199,10 +195,10 @@ for i_dx, dx in enumerate(DXs):
 
         dim = PDE_PNMOL.y0.size
         print(
-            f"\n======| Experiment {i_exp} of {num_exp_total} +++ dt={dt}, dx={dx} (state dimension: {dim} = 3 * {dim//3}) \n"
+            f"\n======| Experiment {i_exp} of {num_exp_total} +++ dt={dt}, dx={dx} (state dimension: {dim} = 3 * {dim/3}) \n"
         )
 
-        mean_white, std_white, elapsed_time_white = solve_pde_pnmol_white(
+        mean_white, std_white, cov_white, elapsed_time_white = solve_pde_pnmol_white(
             PDE_PNMOL,
             dt=dt,
             nu=NUM_DERIVATIVES,
@@ -210,20 +206,43 @@ for i_dx, dx in enumerate(DXs):
             kernel=KERNEL_DIFFUSION_PNMOL,
         )
 
-        mean_tornadox, std_tornadox, elapsed_time_tornadox = solve_pde_tornadox(
+        (
+            mean_tornadox,
+            std_tornadox,
+            cov_tornadox,
+            elapsed_time_tornadox,
+        ) = solve_pde_tornadox(
             PDE_PNMOL, dt=dt, nu=NUM_DERIVATIVES, progressbar=PROGRESSBAR
         )
-        mean_reference, std_reference, elapsed_time_reference = solve_pde_reference(
+        (
+            mean_reference,
+            std_reference,
+            cov_reference,
+            elapsed_time_reference,
+        ) = solve_pde_reference(
             PDE_REFERENCE,
             high_res_factor_dx=HIGH_RES_FACTOR_DX,
         )
 
-        error_white = jnp.linalg.norm(
-            (mean_white - mean_reference) / mean_reference
-        ) / jnp.sqrt(mean_reference.size)
+        error_white_abs = jnp.abs(mean_white - mean_reference)
+        error_tornadox_abs = jnp.abs(mean_tornadox - mean_reference)
+        error_white = jnp.linalg.norm(error_white_abs / mean_reference) / jnp.sqrt(
+            mean_reference.size
+        )
         error_tornadox = jnp.linalg.norm(
-            (mean_tornadox - mean_reference) / mean_reference
+            error_tornadox_abs / mean_reference
         ) / jnp.sqrt(mean_reference.size)
+
+        chi2_white = (
+            error_white_abs
+            @ jnp.linalg.solve(cov_white, error_white_abs)
+            / error_white_abs.size
+        )
+        chi2_tornadox = (
+            error_tornadox_abs
+            @ jnp.linalg.solve(cov_tornadox, error_tornadox_abs)
+            / error_tornadox_abs.size
+        )
 
         mean_std_white = jnp.mean(std_white)
         mean_std_tornadox = jnp.mean(std_tornadox)
@@ -231,12 +250,14 @@ for i_dx, dx in enumerate(DXs):
         RESULT_WHITE["error"][i_dx, i_dt] = error_white
         RESULT_WHITE["std"][i_dx, i_dt] = mean_std_white
         RESULT_WHITE["runtime"][i_dx, i_dt] = elapsed_time_white
+        RESULT_WHITE["chi2"][i_dx, i_dt] = chi2_white
         RESULT_WHITE["dt"][i_dx, i_dt] = dt
         RESULT_WHITE["dx"][i_dx, i_dt] = dx
 
         RESULT_TORNADOX["error"][i_dx, i_dt] = error_tornadox
         RESULT_TORNADOX["std"][i_dx, i_dt] = mean_std_tornadox
         RESULT_TORNADOX["runtime"][i_dx, i_dt] = elapsed_time_tornadox
+        RESULT_WHITE["chi2"][i_dx, i_dt] = chi2_tornadox
         RESULT_TORNADOX["dt"][i_dx, i_dt] = dt
         RESULT_TORNADOX["dx"][i_dx, i_dt] = dx
 
